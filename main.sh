213a53722c8417b451a6843ac506827d8ad6435d
sksayz5 <137992110+sksayz5@users.noreply.github.com>
2024-09-04 16:49:48 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -22,7 +22,8 @@ DEVICE_DEFCONFIG="sdm670-perf_defconfig"
 COMMON_DEFCONFIG=""
 DEVICE_ARCH="arch/arm64"
 
-
+# Clang
+CLANG_REPO="kdrag0n/proton-clang"
 
 # ------------------------------------------------------------
 
@@ -76,6 +77,7 @@ KERNEL_SOURCE="${KERNEL_REPO::-1}/tree/$KERNEL_BRANCH"
 KERNEL_DIR="$WORKDIR/$KERNEL_NAME"
 
 KERNELSU_SOURCE="https://github.com/$KERNELSU_REPO"
+CLANG_SOURCE="https://github.com/$CLANG_REPO"
 README="https://github.com/silvzr/bootlegger_kernel_archive/blob/master/README.md"
 
 if [[ ! -z "$COMMON_DEFCONFIG" ]]; then
